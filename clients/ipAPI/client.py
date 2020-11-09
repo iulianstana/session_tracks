284e75cef68c01c8360a54b41cc04256cbdc3451
Iulian Stana <julian.stana@gmail.com>
2020-11-09 21:14:11 +0200

    Adding Ip API client

---
+++ clients/ipAPI/client.py
@@ -0,0 +1,29 @@
+import requests
+
+
+default_fields = 'status,message,continent,country,countryCode,region,regionName,city,lat,lon,timezone,query'
+
+
+class IpAPIClient(object):
+    def __init__(self, origin, fields=None):
+        self.origin = origin
+        self.base_request = 'http://ip-api.com/json/'
+        self.fields = fields if fields else default_fields
+
+    def get(self, ip):
+        req = self.do_request(ip)
+        if req.status_code != 200:
+            return {
+                'errors': [req.content]
+            }, 400
+
+        content = req.json()
+        if content['status'] != 'success':
+            return {
+                'errors': [content]
+            }, 400
+        
+        return content, 200
+
+    def do_request(self, ip):
+        return requests.get('{0}{1}?fields={2}'.format(self.base_request, ip, self.fields))
